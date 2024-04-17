14836db904431c2b34774d49ef03931ace30615f
Emir Selengil <emirselengil26@gmail.com>
2024-04-17 19:56:49 +0300

    Add files via upload

---
+++ Lesson 5/Exp 1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace InClass_152120211124_152120211106_Group3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.isim = new System.Windows.Forms.TextBox();
+            this.Tel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.yemek = new System.Windows.Forms.ComboBox();
+            this.sehir = new System.Windows.Forms.ComboBox();
+            this.siparis = new System.Windows.Forms.ComboBox();
+            this.ilce = new System.Windows.Forms.ComboBox();
+            this.adet = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.adet)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // isim
+            // 
+            this.isim.Location = new System.Drawing.Point(58, 115);
+            this.isim.Multiline = true;
+            this.isim.Name = "isim";
+            this.isim.Size = new System.Drawing.Size(160, 21);
+            this.isim.TabIndex = 0;
+            this.isim.WordWrap = false;
+            this.isim.TextChanged += new System.EventHandler(this.isim_TextChanged);
+            // 
+            // Tel
+            // 
+            this.Tel.Location = new System.Drawing.Point(58, 168);
+            this.Tel.Multiline = true;
+            this.Tel.Name = "Tel";
+            this.Tel.Size = new System.Drawing.Size(160, 21);
+            this.Tel.TabIndex = 1;
+            this.Tel.TextChanged += new System.EventHandler(this.Tel_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(55, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Müşteri Ad-Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(55, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Müşteri Telefon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(310, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Adres İl";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(310, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Adres İlçe";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(306, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Siparişlerim";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(192, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(304, 25);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "YEMEK SİPARİŞ SİSTEMİ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 36);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Sipariş Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(55, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 16);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Yemek Türü";
+            // 
+            // yemek
+            // 
+            this.yemek.FormattingEnabled = true;
+            this.yemek.Location = new System.Drawing.Point(58, 244);
+            this.yemek.Name = "yemek";
+            this.yemek.Size = new System.Drawing.Size(160, 21);
+            this.yemek.TabIndex = 23;
+            // 
+            // sehir
+            // 
+            this.sehir.FormattingEnabled = true;
+            this.sehir.Items.AddRange(new object[] {
+            ""});
+            this.sehir.Location = new System.Drawing.Point(296, 115);
+            this.sehir.Name = "sehir";
+            this.sehir.Size = new System.Drawing.Size(133, 21);
+            this.sehir.TabIndex = 24;
+            this.sehir.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // siparis
+            // 
+            this.siparis.FormattingEnabled = true;
+            this.siparis.Location = new System.Drawing.Point(296, 220);
+            this.siparis.Name = "siparis";
+            this.siparis.Size = new System.Drawing.Size(257, 21);
+            this.siparis.TabIndex = 25;
+            this.siparis.SelectedIndexChanged += new System.EventHandler(this.siparis_SelectedIndexChanged);
+            // 
+            // ilce
+            // 
+            this.ilce.FormattingEnabled = true;
+            this.ilce.Location = new System.Drawing.Point(296, 168);
+            this.ilce.Name = "ilce";
+            this.ilce.Size = new System.Drawing.Size(133, 21);
+            this.ilce.TabIndex = 26;
+            // 
+            // adet
+            // 
+            this.adet.Location = new System.Drawing.Point(58, 289);
+            this.adet.Name = "adet";
+            this.adet.Size = new System.Drawing.Size(160, 20);
+            this.adet.TabIndex = 28;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(13, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 14);
+            this.label8.TabIndex = 29;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(296, 261);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 165);
+            this.panel1.TabIndex = 30;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(13, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(146, 14);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Seçilen Sipariş Detayı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(683, 453);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.adet);
+            this.Controls.Add(this.ilce);
+            this.Controls.Add(this.siparis);
+            this.Controls.Add(this.sehir);
+            this.Controls.Add(this.yemek);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tel);
+            this.Controls.Add(this.isim);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.adet)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox isim;
+        private System.Windows.Forms.TextBox Tel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox yemek;
+        private System.Windows.Forms.ComboBox sehir;
+        private System.Windows.Forms.ComboBox siparis;
+        private System.Windows.Forms.ComboBox ilce;
+        private System.Windows.Forms.NumericUpDown adet;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Lesson 5/Exp 1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace InClass_152120211124_152120211106_Group3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] yemekler = { "Hamburger", "Sandviç", "Zurna Dürüm", "Balaban Kebap", "Kokoreç", "İskender", "Cantik" };
+            foreach (string item in yemekler)
+            {
+                yemek.Items.Add(item); 
+            }
+
+            sehir.Items.Clear();
+            string[] iller = { "Eskişehir", "Bursa" };
+            foreach (string item in iller)
+            {
+                sehir.Items.Add(item);
+            }
+
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(sehir.SelectedIndex == 0)
+            {
+                ilce.Text = "";
+                ilce.Items.Clear();
+                string[] ilceler = { "Alpu", "Beylikova","Çifteler", "Günyüzü", "Han", "İnönü", "Mahmudiye", "Mihalgazi", "Mihalıççık", "Tepebaşı", "Sarıcakaya", "Seyitgazi", "Sivrihisar" , "Odunpazarı" };
+                foreach(string item in ilceler)
+                {
+                    ilce.Items.Add(item);
+                }
+            }
+            else
+            {
+                ilce.Text = "";
+                ilce.Items.Clear();
+                string[] ilceler = { "Gürsu", "Kestel","Nilüfer", "Osmangazi", "Yıldırım", "Büyükorhan", "Gemlik", "Harmancık", "İnegöl", "İznik", "Karacabey", "Keles", "Mudanya", "Mustafakemalpaşa", "Orhaneli", "Orhangazi", "Yenişehir" };
+                foreach (string item in ilceler)
+                {
+                    ilce.Items.Add(item);
+                }
+                
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(isim.Text != "" && Tel.Text!= "" && yemek.SelectedItem.ToString() != "" && adet.Value.ToString() != "" && sehir.SelectedItem.ToString() != "" && ilce.SelectedItem.ToString() != "")
+            {
+                string text = DateTime.Now + " tarihli siparişiniz oluşturuldu: " + "," + isim.Text + "," + Tel.Text + "," + yemek.SelectedItem.ToString() + "," + adet.Value.ToString() + "," + sehir.SelectedItem.ToString() + "," + ilce.SelectedItem.ToString();
+                siparis.Items.Add(text);
+            }
+            else
+            {
+                MessageBox.Show("Fill all blanks", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void siparis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label8.Text = "";
+            string[] siparis_ = siparis.SelectedItem.ToString().Split(',');
+            
+            foreach (string siparis_item in siparis_)
+            {
+                label8.Text += siparis_item+ Environment.NewLine;
+            }
+            
+        }
+
+        private bool IsNumeric(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (!char.IsDigit(c) || char.IsWhiteSpace(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool IsAlphabetic(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (!char.IsLetter(c) || char.IsWhiteSpace(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void isim_TextChanged(object sender, EventArgs e)
+        {
+            if (!IsAlphabetic(isim.Text))
+            {
+                MessageBox.Show("Invalid Charecter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                isim.Text = string.Empty;
+            }
+        }
+
+        private void Tel_TextChanged(object sender, EventArgs e)
+        {
+            if (!IsNumeric(Tel.Text))
+            {
+                MessageBox.Show("Invalid Charecter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Tel.Text = string.Empty;
+            }
+        }
+    }
+}
